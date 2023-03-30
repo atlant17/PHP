5bf93cee65a7efbaf2b1265ad96118a13223e40e
Aleksei Tumanov <atlantaleksey@gmail.com>
2023-03-30 11:44:15 +0300

    commit module_12

---
+++ 10/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div>
+                <h3>Задание 1</h3>
+                <?php require "tables/table1.php" ?>
+                <p class="additional"><h4>Выводы о сравнении в "PHP"</h4>
+                <br>
+                При работе с <i>логическими операторами</i>, наблюдается прямая взаимосвяь с языком <i."JavaScript"</i>, 
+                за исключением оператора <strong>"XOR"</strong>, который в языке <strong>"PHP"</strong>, выставляет условие: <i?>одно из значений является 'true'.</i>
+            </div>
+            <div>
+                <div>
+                    <p>
+                    <h3>Задание 2</h3>
+                    <?php require "tables/table2_1.php" ?>
+                    <p class="paragraph">таблица 1</p>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <?php require "tables/table2_2.php" ?>
+                    <!-- <p class="paragraph">таблица 2</p> -->
+                </div>
+            </div>
+            <div>
+                <p class="additional"><h4>Выводы о сравнении в "PHP"</h4>
+                <br>
+                По итогам данного модуля я узнал и применил на практике: <i>операторы сравнения, логические операторы, и отличие строго равенства от не строгого</i>. 
+                Также, мною было показно на практикe сравнение различных типов, <i>со строгим и не строгим равенством</i>: 
+                <ul><i>
+                    <li>null</li>
+                    <li>bool</li>
+                    <li>string</li>
+                    <li>number</li>
+                    <li>string(number)</li></i>
+                </ul>
+                По итогу данной работы сделан вывод:
+                <br> 
+                При <i><strong>НЕ</strong> Строгом равенстве</i>, при сравнении значения <strong>"true"</strong> с различными типами, выражение является верным, если мы приравниваем его к <strong>true, не пустой строке  и единице (независимо от знака)</strong>. <br>
+                Выражение является ложным если мы приравниваем значение к <strong>"false", нулю , "null" и пустой строке.</strong> 
+                <br>
+                В отличие от значения <i>"true"</i> и <i>"false"</i>, значение <strong>'number не 0'</strong> является верным, если оно приравнивается к <strong>'true', такому же (не отрицательному числу) и строке с таким же значением.</strong>  
+                Значение <strong>'number 0'</strong> является верным, если оно приравнивается к <strong>"false", '0' и "null".</strong>
+                <br>
+                Строка, при <i><strong>НЕ</strong> Строгом равенстве</i>, является верным выражением, при приравнивании к значениям <strong> "true" и такому же значению в строке. </strong>
+                <br>
+                <br>
+                При <i>Строгом равенстве</i>, не зависимо от типа значения, значение будет являтся верным, только если он идентично приравниваемому.
+                </p>
+            </div>
+        </div>
+    </main>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,65 +1,208 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
-</head>
-<body>
-    <main>
-        <div class="container">
-            <div>
-                <h3>Задание 1</h3>
-                <?php require "tables/table1.php" ?>
-                <p class="additional"><h4>Выводы о сравнении в "PHP"</h4>
-                <br>
-                При работе с <i>логическими операторами</i>, наблюдается прямая взаимосвяь с языком <i."JavaScript"</i>, 
-                за исключением оператора <strong>"XOR"</strong>, который в языке <strong>"PHP"</strong>, выставляет условие: <i?>одно из значений является 'true'.</i>
-            </div>
-            <div>
-                <div>
-                    <p>
-                    <h3>Задание 2</h3>
-                    <?php require "tables/table2_1.php" ?>
-                    <p class="paragraph">таблица 1</p>
-                    </p>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <?php require "tables/table2_2.php" ?>
-                    <!-- <p class="paragraph">таблица 2</p> -->
-                </div>
-            </div>
-            <div>
-                <p class="additional"><h4>Выводы о сравнении в "PHP"</h4>
-                <br>
-                По итогам данного модуля я узнал и применил на практике: <i>операторы сравнения, логические операторы, и отличие строго равенства от не строгого</i>. 
-                Также, мною было показно на практикe сравнение различных типов, <i>со строгим и не строгим равенством</i>: 
-                <ul><i>
-                    <li>null</li>
-                    <li>bool</li>
-                    <li>string</li>
-                    <li>number</li>
-                    <li>string(number)</li></i>
-                </ul>
-                По итогу данной работы сделан вывод:
-                <br> 
-                При <i><strong>НЕ</strong> Строгом равенстве</i>, при сравнении значения <strong>"true"</strong> с различными типами, выражение является верным, если мы приравниваем его к <strong>true, не пустой строке  и единице (независимо от знака)</strong>. <br>
-                Выражение является ложным если мы приравниваем значение к <strong>"false", нулю , "null" и пустой строке.</strong> 
-                <br>
-                В отличие от значения <i>"true"</i> и <i>"false"</i>, значение <strong>'number не 0'</strong> является верным, если оно приравнивается к <strong>'true', такому же (не отрицательному числу) и строке с таким же значением.</strong>  
-                Значение <strong>'number 0'</strong> является верным, если оно приравнивается к <strong>"false", '0' и "null".</strong>
-                <br>
-                Строка, при <i><strong>НЕ</strong> Строгом равенстве</i>, является верным выражением, при приравнивании к значениям <strong> "true" и такому же значению в строке. </strong>
-                <br>
-                <br>
-                При <i>Строгом равенстве</i>, не зависимо от типа значения, значение будет являтся верным, только если он идентично приравниваемому.
-                </p>
-            </div>
-        </div>
-    </main>
-    </body>
-</html>
+<?php
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+?>
+
+<?php
+
+    $i = 0;
+    $sumMen;
+    $sumWomen;
+    $sumNone;
+    $arrGen = [];
+  
+    
+    foreach($example_persons_array as $key){
+        // разбитие массива
+        $string = $example_persons_array[$i]["fullname"];
+        getPartsFromFullname($string);
+        echo '<br>';
+        $surname = getSurname($string);
+        echo '<br>';
+        $name = getName($string);
+        echo '<br>';
+        $patronymic = getPatronymic($string);
+        echo '<br>';
+        echo getFullnameFromParts($surname, $name, $patronymic);
+        echo getShortName($string);
+        $gensum = getGenderFromName($string);
+        echo getGenderFromName($string);
+        echo '<br>';
+        $i++;
+        if(!empty($key['fullname']) && is_array($key['fullname'])){ 
+            $i += ($key['fullname']); 
+        }
+    }
+
+    // разделения массива на 3 элемента
+    function getPartsFromFullname($string) {
+        $arrayFullname = ['surname', 'name', 'patronymic'];
+        $stringPartsName = explode(' ', $string);
+        $array = (array_combine($arrayFullname, $stringPartsName));
+        echo '<br>';
+        return $array;
+    }
+    // Фамилия
+    function getSurname($string){
+        $stringPartsName = explode(' ', $string);
+        $surname = $stringPartsName[0];
+        echo $surname; 
+        return $surname;
+    }
+    // Имя
+    function getName($string){
+        $stringPartsName = explode(' ', $string);
+        $name = $stringPartsName[1];
+        echo $name; 
+        return $name;
+    }
+    // Отчество
+    function getPatronymic($string){
+        $stringPartsName = explode(' ', $string);
+        $patronymic = $stringPartsName[2];
+        echo $patronymic; 
+        return $patronymic;
+    }
+    // Полное ФИО
+    function getFullnameFromParts($surname, $name, $patronymic){
+        $fullname = $surname . ' ' . $name . ' ' . $patronymic;
+        // echo $fullname;
+        return $fullname;
+    }
+    // Фамилия И.
+    function getShortName($string){
+        $arr = getPartsFromFullname($string);
+        $surname1 = $arr["surname"];
+        $name1 = $arr["name"];
+        $surname1 = mb_substr($surname1, 0, 1);
+        $nameSurname = $name1 . ' ' . $surname1 . '.';
+        return $nameSurname;
+    }
+    // Гендер
+    function getGenderFromName($string){
+        $gensum = 0;
+        global $arrGen;
+        global $i;
+        $arr = getPartsFromFullname($string);
+        $surname1 = $arr["surname"];
+        $name1 = $arr["name"];
+        $patronymic1 = $arr["patronymic"];
+        if (mb_substr($surname1,-2) == 'ва'){
+            $gensum--;
+        }elseif(mb_substr($surname1,-1) == 'в'){
+            $gensum++;
+        }
+        if (mb_substr($name1,-1) == 'а'){
+            $gensum--;
+        }elseif(mb_substr($name1,-1) == 'й'|| mb_substr($name1,-1) == 'н'){
+                $gensum++;
+        }
+        if (mb_substr($patronymic1, -3) == 'вна'){
+            $gensum--;
+        } elseif(mb_substr($patronymic1, -2) == 'ич'){
+                    $gensum++;
+        } 
+        if ($gensum > 0) {
+            $gensum = 1 . ' ' . '(мужской пол)';
+            global $sumMen;
+            $sumMen++;
+            array_push($arrGen, 1);
+        } elseif ($gensum < 0) {
+            $gensum = -1 . ' ' . '(женский пол)';
+            global $sumWomen;
+            $sumWomen++;
+            array_push($arrGen, -1);
+        } else {
+           $gensum = 0 . ' ' . '(неопределенный пол)';
+           global $sumNone;
+           $sumNone++;
+           array_push($arrGen, 0);
+        }
+        // echo $gensum;
+        return $gensum;
+    }   
+    echo '<br>';  
+// Выполнено не так как требует задание
+    function getGenderDescription(){
+        global $arrGen;
+        global $sumMen;
+        global $sumWomen;
+        global $sumNone;
+        $genderM = ($sumMen / count($arrGen)) * 100;
+        $genderW = ($sumWomen / count($arrGen)) * 100;
+        $genderN = ($sumNone / count($arrGen)) * 100;
+        echo 'Гендерный состав аудитории:' . '<br>';
+        echo '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ' . '<br>';
+        echo 'Мужчины - ' . round($genderM, 1) . '%' . '<br>';
+        echo 'Женщины - ' . round($genderW, 1) . '%' .'<br>';
+        echo 'Не удалось определить - ' . round($genderN, 1) . '%' . '<br>';
+        
+    }
+    echo '<br>';
+    echo '<br>';
+    getGenderDescription();
+  
+   function getPerfectPartner($surname, $name, $patronymic, $persons_array){
+    $surname = mb_convert_case($surname, MB_CASE_TITLE_SIMPLE);
+    $name = mb_convert_case($name, MB_CASE_TITLE_SIMPLE);
+    $patronymic = mb_convert_case($patronymic, MB_CASE_TITLE_SIMPLE);
+    $fullname = getFullnameFromParts($surname, $name, $patronymic);
+    $gender = getGenderFromName($fullname);
+    $partnerGender = $gender;
+    while((int)$partnerGender + (int)$gender !== 0){
+        $partnerFullname = $persons_array[array_rand($persons_array, 1)]['fullname'];
+        $partnerGender = getGenderFromName($partnerFullname);
+    } 
+    echo 'Идеальная пара' . '<br>';
+    echo '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ';
+    echo $name . ' ' . mb_substr($surname, 0, 1) . '. ' . ' + ' .  getShortName($partnerFullname) . '<br>';
+    echo '♡ Идеально на ' . rand(5000, 10000) / 100 . '% ♡';
+
+   }
+   getPerfectPartner('сеРгЕев', 'ваСИлий', 'степАНоВич', $example_persons_array);
+   
+?>
